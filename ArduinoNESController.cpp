e209dfbd0d74cc55c2be49446a0fc9e9c65b4f5a
aaronshappell <crumster88@gmail.com>
2017-08-01 20:34:56 +0300

    Finish lazy methods

--- ArduinoNESController.cpp
+++ ArduinoNESController.cpp
@@ -32,73 +32,97 @@ bool ArduinoNESController::isButtonUp(int button){
 }
 
 bool ArduinoNESController::isButtonPressedA(){
+    isButtonPressed(0);
 }
 
 bool ArduinoNESController::isButtonPressedB(){
+    isButtonPressed(1);
 }
 
 bool ArduinoNESController::isButtonPressedSelect(){
+    isButtonPressed(2);
 }
 
 bool ArduinoNESController::isButtonPressedStart(){
+    isButtonPressed(3);
 }
 
 bool ArduinoNESController::isButtonPressedUp(){
+    isButtonPressed(4);
 }
 
 bool ArduinoNESController::isButtonPressedDown(){
+    isButtonPressed(5);
 }
 
 bool ArduinoNESController::isButtonPressedLeft(){
+    isButtonPressed(6);
 }
 
 bool ArduinoNESController::isButtonPressedRight(){
+    isButtonPressed(7);
 }
 
 bool ArduinoNESController::isButtonDownA(){
+    isButtonDown(0);
 }
 
 bool ArduinoNESController::isButtonDownB(){
+    isButtonDown(1);
 }
 
 bool ArduinoNESController::isButtonDownSelect(){
+    isButtonDown(2);
 }
 
 bool ArduinoNESController::isButtonDownStart(){
+    isButtonDown(3);
 }
 
 bool ArduinoNESController::isButtonDownUp(){
+    isButtonDown(4);
 }
 
 bool ArduinoNESController::isButtonDownDown(){
+    isButtonDown(5);
 }
 
 bool ArduinoNESController::isButtonDownLeft(){
+    isButtonDown(6);
 }
 
 bool ArduinoNESController::isButtonDownRight(){
+    isButtonDown(7);
 }
 
 bool ArduinoNESController::isButtonUpA(){
+    isButtonUp(0);
 }
 
 bool ArduinoNESController::isButtonUpB(){
+    isButtonUp(1);
 }
 
 bool ArduinoNESController::isButtonUpSelect(){
+    isButtonUp(2);
 }
 
 bool ArduinoNESController::isButtonUpStart(){
+    isButtonUp(3);
 }
 
 bool ArduinoNESController::isButtonUpUp(){
+    isButtonUp(4);
 }
 
 bool ArduinoNESController::isButtonUpDown(){
+    isButtonUp(5);
 }
 
 bool ArduinoNESController::isButtonUpLeft(){
+    isButtonUp(6);
 }
 
 bool ArduinoNESController::isButtonUpRight(){
+    isButtonUp(7);
 }
