db59c1ace062f759799c9f0170c74734525f5ee5
aaronshappell <crumster88@gmail.com>
2017-08-01 22:51:57 +0300

    Finish example

--- examples/PollInput/PollInput.ino
+++ examples/PollInput/PollInput.ino
@@ -1,7 +1,35 @@
-void setup(){
+#include <"ArduinoNESController.h">
 
+//Define pins
+int latchPin = 9;
+int clockPin = 10;
+int dataPin = 11;
+
+ArduinoNESController controller(latchPin, clockPin, dataPin);
+
+void setup(){
+    Serial.begin(9600);
 }
 
 void loop(){
-    
+    //Update the controller
+    controller.update();
+
+    //Check if a button has been pressed
+    if(controller.isButtonPressedA()){
+        Serial.println("Button A has been Pressed!");
+    }
+
+    //Check if a button is down
+    if(controller.isButtonDownA()){
+        Serial.println("Button A is down!");
+    }
+
+    //Check if a button is up
+    if(controller.isButtonUpA()){
+        Serial.println("Button A is up!");
+    }
+
+    //Get byte that represents all button states
+    byte buttons = controller.getButtons();
 }
